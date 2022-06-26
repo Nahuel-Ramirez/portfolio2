91c3917801cfd70ffc18cad010bee48d34d1f145
Nahuel-Ramirez <nahuel-ramirez@hotmail.com>
2022-06-26 18:15:10 -0300

    separacion de paginas y creacion de contact

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const urlBase = "api.github.com";
+import Swal from "sweetalert2";
 
-// const getRepos = async () => {
-//   await fetch(urlBase + `/users/Nahuel-Ramirez/repos`, {
-//     headers: {
-//       accept: "application/vnd.github.v3+json",
-//       "Access-Control-Allow-Origin": "*",
-//     },
-//   })
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch((err) => console.log(err));
-// };
+const regexEmail =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-// getRepos();
-
-const reloadPage = document.getElementById("portfolio");
-
-console.log(reloadPage);
+Swal.fire({
+  title: "¡Atencion!",
+  text: "Debe rellenar todos los campos",
+  icon: "warning",
+  timer: 2000,
+});
